d5d7e87789638c5ffa3f34518c69859d44ad167c
Steve <itsmesteve88@yahoo.com>
2022-12-01 17:11:56 +0000

    Css

--- src/Components/Home/ProductCard/ProductCard.js
+++ src/Components/Home/ProductCard/ProductCard.js
@@ -49,7 +49,7 @@ function ProductCard(values) {
           <span>{values.item.rating.count}</span>
         </p>
         <p className="card-text">{values.item.description}</p>
-        <a href="#" className="btn btn-success flex-box" onClick={AddtoCart}>
+        <button href="#" className="btn btn-success flex-box" onClick={AddtoCart}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -64,7 +64,7 @@ function ProductCard(values) {
             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
           </svg>
           &nbsp;Add To Cart
-        </a>
+        </button>
       </div>
     </div>
   );
